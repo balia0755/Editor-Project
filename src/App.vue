471c677dd158d934d95eb45e4edc5fb8670034df
Edward Huang <balian0755@gmail.com>
2025-07-07 16:20:03 +0800

    minor update

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,21 @@
   <div id="app">
     <tiptap />
   </div>
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
 </template>
 
 <script>
   import Tiptap from './components/Tiptap.vue'
-
   export default {
     name: 'App',
     components: {
       Tiptap,
+    },
+    data() {
+      return {
+        rawHtml: '<span style="color: red;">这是红色文本</span>'
+      }
     },
   }
 </script>
